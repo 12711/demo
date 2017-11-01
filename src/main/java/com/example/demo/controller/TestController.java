f4acd816b5701f37b4f0c9cd45e2959e6efd4304
lsm <1076396468@qq.com>
2017-11-02 00:07:57 +0800

    first init

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public Map test(){
+        Map data = new HashMap();
+        data.put("code",200);
+        data.put("msg","this is a demo");
+        System.out.printf("kkkkkkkkk");
+        return data;
+    }
+}
